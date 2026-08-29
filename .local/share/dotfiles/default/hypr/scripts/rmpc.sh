6a3f199a3e9030a4e69a40d84b358e541516cbac
GhostGamer6969 <arambhmani@hotmail.com>
2026-08-29 08:30:53 +0530

    looks update

--- .local/share/dotfiles/default/hypr/scripts/rmpc.sh
+++ .local/share/dotfiles/default/hypr/scripts/rmpc.sh
@@ -3,5 +3,5 @@ if hyprctl clients -j | jq -r '.[].title' | grep -qi "rmpc"; then
   hyprctl dispatch focuswindow title:rmpc > /dev/null
 else
   # ghostty -e rmpc & > /dev/null
-  ghostty --class=local.floating_rmpc -e rmpc & > /dev/null
+  ghostty --class=local.floating_wide -e rmpc & > /dev/null
 fi
