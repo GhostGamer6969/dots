e1bea1bcbfaa961e4bfa932f2a65c698b5924559
GhostGamer6969 <arambhmani@hotmail.com>
2026-08-29 05:42:58 +0530

    rmpc floating
    rmpc transparent

--- .local/share/dotfiles/default/hypr/scripts/rmpc.sh
+++ .local/share/dotfiles/default/hypr/scripts/rmpc.sh
@@ -2,5 +2,6 @@
 if hyprctl clients -j | jq -r '.[].title' | grep -qi "rmpc"; then
   hyprctl dispatch focuswindow title:rmpc > /dev/null
 else
-  ghostty -e rmpc & > /dev/null
+  # ghostty -e rmpc & > /dev/null
+  ghostty --class=local.floating_rmpc -e rmpc & > /dev/null
 fi
